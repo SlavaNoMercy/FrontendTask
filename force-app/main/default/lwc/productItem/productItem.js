467d10b0415aef82e481c8c0c8e54c31b43ae431
SlavaNoMercy <slava.nomercy@gmail.com>
2022-06-21 16:32:52 +0300

    trying to make product list page selector

--- force-app/main/default/lwc/productItem/productItem.js
+++ force-app/main/default/lwc/productItem/productItem.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class ProductItem extends LightningElement {}
+export default class ProductItem extends LightningElement {
+    @api product;
+
+    selectProduct(){
+        const selectEvent = new CustomEvent('select', {
+            detail: this.product.Id
+        });
+        // Fire the custom event
+        this.dispatchEvent(selectEvent);
+        this.template.querySelector(['data-id="symbol"']).className('element-symbol-hide');
+        //symbol.remove('element-symbol');
+        //symbol.add('element-symbol-hide');
+        this.template.querySelector(['data-id="message"']).className('element-message');
+        //message.remove('element-message-hide');
+        //message.add('element-message');
+    }
+}
--- force-app/main/default/lwc/productSelection/productSelection.js
+++ force-app/main/default/lwc/productSelection/productSelection.js
@@ -1,9 +1,52 @@
-import { LightningElement, wire } from 'lwc';
-import { getProducts } from '@salesforce/apex/SomeCompanyController.getProducts';
+import { LightningElement, track } from "lwc";
 
 export default class ProductSelection extends LightningElement {
-    selectedProduct;
-    @wire(getProducts) products;
+  selectedProduct;
+  @track msg;
+  products = [
+    {
+      Id: "1stProduct",
+      title: "GLOBAL COMPANY PAYMENT SYSTEM",
+      symbol: "$",
+      message: "3q",
+      symbolcls: true,
+      messagecls: false
+    },
+    {
+      Id: "2ndProduct",
+      title: "GLOBAL COMPANY MAILING SYSTEM",
+      symbol: "@",
+      message: "3w",
+      symbolcls: true,
+      messagecls: false
+    },
+    {
+      Id: "3rdProduct",
+      title: "GLOBAL COMPANY INDEX SYSTEM",
+      symbol: "#",
+      message: "3e",
+      symbolcls: true,
+      messagecls: false
+    }
+  ];
+  
+  constructor() {
+      super();   
+      this.template.addEventListener('select', this.handleSelect.bind(this));
+  }
 
-    
+  handleSelect(event){
+    const textVal = event.detail;
+    this.msg = textVal;
+    this.selectProduct(this.products,this.msg);
+  }
+
+  selectProduct(array,elementId){
+    array.forEach(element => {
+        if(element.Id===elementId){
+            this.selectedProduct = element;
+        }
+    });
+  }
+
 }
