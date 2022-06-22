d37f86fb01bd57cebd630d27b8255e6fe788bef7
SlavaNoMercy <slava.nomercy@gmail.com>
2022-06-22 18:07:33 +0300

    switch components + footer + styles

--- force-app/main/default/lwc/mainComponent/mainComponent.js
+++ force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class MainComponent extends LightningElement {}
+export default class MainComponent extends LightningElement {
+    @track state = {
+        firstPage:true,
+        secondPage:false
+    }
+
+    handleNextPageEvent(){
+        this.state.firstPage = false;
+        this.state.secondPage = true;
+    }
+
+    handlePrevPageEvent(){
+        this.state.firstPage = true;
+        this.state.secondPage = false;
+    }
+}
