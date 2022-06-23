32b66ff7f0af5ee3dfe8bab037ffcbc0cf848fea
SlavaNoMercy <slava.nomercy@gmail.com>
2022-06-23 14:54:54 +0300

    products switcher debug

--- force-app/main/default/lwc/productItem/productItem.js
+++ force-app/main/default/lwc/productItem/productItem.js
@@ -2,11 +2,19 @@ import { api, LightningElement } from 'lwc';
 
 export default class ProductItem extends LightningElement {
     @api product;
+    @api selectedProduct;
+
+    get isSelected(){
+        if(this.product.Id===this.selectedProduct.Id){
+            return true;
+        }
+        return false;
+    }
 
     selectProduct(){
         const selectEvent = new CustomEvent('select', {
-            detail: this.product
+            detail: this.product.Id
         });
         this.dispatchEvent(selectEvent);
-    }
+    } 
 }
--- force-app/main/default/lwc/productSelection/productSelection.js
+++ force-app/main/default/lwc/productSelection/productSelection.js
@@ -1,46 +1,44 @@
 import { LightningElement, track } from "lwc";
 
 export default class ProductSelection extends LightningElement {
-  selectedProduct;
-  @track msg;
-  products = [
+  @track selectedProduct = {Id:"dummyId"};
+  @track products = [
     {
       Id: "1stProduct",
       title: "GLOBAL COMPANY PAYMENT SYSTEM",
       symbol: "$",
-      message: "3q",
+      message: "3q"
     },
     {
       Id: "2ndProduct",
       title: "GLOBAL COMPANY MAILING SYSTEM",
       symbol: "@",
-      message: "3w",
+      message: "3w"
     },
     {
       Id: "3rdProduct",
       title: "GLOBAL COMPANY INDEX SYSTEM",
       symbol: "#",
-      message: "3e",
+      message: "3e"
     }
   ];
-  
+
   constructor() {
-      super();   
-      this.template.addEventListener('select', this.handleSelect.bind(this));
+    super();
+    this.template.addEventListener("select", this.handleSelect.bind(this));
   }
 
-  handleSelect(event){
+  handleSelect(event) {
     const textVal = event.detail;
-    this.msg = textVal;
-    this.selectProduct(this.products,this.msg);
+    this.choseProduct(this.products, textVal);
   }
 
-  selectProduct(array,elementId){
-    array.forEach(element => {
-        if(element.Id===elementId){
-            this.selectedProduct = element;
-        }
-    });
+  choseProduct(array, productId) {
+    array.forEach((element) => {
+      if (element.Id === productId) {
+        this.selectedProduct = element;
+        console.log("item selected: " + this.selectedProduct);
+      }
+    })
   }
-
 }
